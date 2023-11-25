38e950cf51dde4d60c5b161408d7a7f037fa1876
Jyothi-03 <129101057+Jyothi-03@users.noreply.github.com>
2023-11-25 17:28:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,3 +11,27 @@ function addTask(){
     }
     inputBox.value=''
 }
+
+lisContainer.addEventListener("click", function(e){
+    if (e.target.tagName==="LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+
+    }else if (e.target.tagName==="SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+    
+}, false);
+
+
+function saveData(){
+    localStorage.setItem("data", lisContainer.innerHTML)
+
+}
+
+function showTask(){
+    lisContainer.innerHTML=localStorage.getItem("data")
+}
+
+showTask();
